7f6ed12a6abbc8bbb92891026b6ab463f3709e3e
Deepak Mehta <dmehta@Ingenuity.local>
2018-05-23 17:55:45 +0530

    Added Param GET method

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.response import Response
-from pyramid.view import view_config
 
 def hello_world(request):
     return {'Hello':'world'}
 
 
+def user_name(request):
+    name=request.matchdict['name']
+    return {'Name':name}
+
+
 if __name__ == '__main__':
     with Configurator() as config:
+        #Routes
         config.add_route('hello', '/')
+        config.add_route('username', '/{name}')
+
+        #Views
         config.add_view(hello_world, route_name='hello', renderer='json')
+        config.add_view(user_name, route_name='username', renderer='json')
+        
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
